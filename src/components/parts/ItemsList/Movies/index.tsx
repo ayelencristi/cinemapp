4265a919b478e48fe3edd1ffde8c5fe96cc04e2f
ayelencristi <ayelendcristi@gmail.com>
2022-02-23 18:36:01 -0300

    movies list

--- src/components/parts/ItemsList/Movies/index.tsx
+++ src/components/parts/ItemsList/Movies/index.tsx
@@ -1,5 +1,29 @@
+import { Button, Card } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import { StarRating } from "../.."
+import { useItems } from "../../../../hooks"
+
 const MoviesList = () => {
 
+    const { items } = useItems()
+    return (
+        <div className="container">
+            <div className="row">
+                {items?.map(item =>
+                    <Card className="col-md-3 mb-4 bg-transparent">
+                        <Link to={`/details/${item.id}`} className="nav-link">
+                            <Card.Img src={`http://image.tmdb.org/t/p/w500${item.poster_path}`} />
+                            <Card.Body className="row">
+                                <Card.Title>{item.title || item.name}</Card.Title>
+                                <StarRating rating={item.vote_average}></StarRating>
+                                <Card.Text>{item.vote_average}</Card.Text>
+                                <Button variant="dark"></Button>
+                            </Card.Body>
+                        </Link>
+                    </Card>)}
+            </div>
+        </div >
+    )
 }
 
 export { MoviesList }
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,4 @@
 export { useUsers } from './useUsers'
 export { useAuth } from './useAuth'
+export { useItems } from './useItems'
+export { useMovies } from './useMovies'
